c56760ce1ad66a0011635b58b12d1505109ef9ef
Thomas Ayson <aysonisaiahthomas@gmail.com>
2020-12-07 19:54:08 +0800

    Initial Commit

---
+++ MyInventory/Controllers/ItemController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyInventory.Data;
+using MyInventory.Models;
+
+namespace MyInventory.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var list = _context.Items.ToList();
+            return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Item record)
+        {
+            var item = new Item();
+            item.Name = record.Name;
+            item.Code = record.Code;
+            item.Description = record.Description;
+            item.Price = record.Price;
+            item.DateAdded = DateTime.Now;
+            item.Type = record.Type;
+
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var item = _context.Items.Where(i => i.ItemID == id).SingleOrDefault();
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        [HttpPost]
+        public IActionResult Edit(int? id, Item record)
+        {
+            var item = _context.Items.Where(i => i.ItemID == id).SingleOrDefault();
+            item.Name = record.Name;
+            item.Code = record.Code;
+            item.Description = record.Description;
+            item.Price = record.Price;
+            item.DateAdded = DateTime.Now;
+            item.Type = record.Type;
+
+            _context.Items.Update(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var item = _context.Items.Where(i => i.ItemID == id).SingleOrDefault();
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyInventory/Models/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyInventory.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+        [Required(ErrorMessage ="Required")]
+        public string Name { get; set; }
+        public string Code { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Required")]
+        public decimal Price { get; set; }
+        [Display(Name = "Date Added")]
+        public DateTime DateAdded { get; set; }
+        [Display(Name = "Date Added")]
+        public DateTime? DateModified { get; set; }
+        [Display(Name = "Item Type")]
+        public ItemType Type { get; set; }
+    }
+    public enum ItemType
+    {
+        RawMaterials = 1,
+        Intermediate = 2
+    }
+}
